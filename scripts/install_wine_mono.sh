8066dfb31c2922d3ea8bc3c57b49b6c85b192d38
Fynn Haupt <info@fynnhaupt.de>
2024-10-24 18:57:32 +0200

    hide wine mono install errors

--- scripts/install_wine_mono.sh
+++ scripts/install_wine_mono.sh
@@ -1,5 +1,5 @@
 echo "Installing Wine Mono..."
 wget https://dl.winehq.org/wine/wine-mono/9.3.0/wine-mono-9.3.0-x86.msi
-wine WINEDEBUG=-all msiexec /i wine-mono-9.3.0-x86.msi /qn
+WINEDEBUG=-all wine msiexec /i wine-mono-9.3.0-x86.msi /qn
 rm wine-mono-9.3.0-x86.msi
 echo "Done..."
