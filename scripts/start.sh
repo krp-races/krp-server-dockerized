976e6999a548c45511427037a9f6b5879e4205eb
Fynn Haupt <info@fynnhaupt.de>
2024-10-24 20:23:23 +0200

    check if krp or wine-mono already installed

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,16 +1,30 @@
-echo "Installing Wine Mono..."
-WINEDEBUG=-all wine msiexec /i wine-mono.msi /qn
-echo "Done..."
+if [ ! -f ".wine-mono-installed" ]; then
+    echo "Installing Wine Mono..."
+    WINEDEBUG=-all wine msiexec /i wine-mono.msi /qn
+    rm wine-mono.msi
+    cat > .wine-mono-installed
+    echo "Done..."
+else
+    echo "Wine Mono already installed..."
+fi
 
 echo "Setup Xvfb..."
 Xvfb :99 -screen 0 1024x768x16 &
 export DISPLAY=:99
 echo "Done..."
 
-echo "Installing KRP Server..."
-wine krp-installer.exe -extract
-unzip krp.zip
-echo "Done..."
+if [ ! -f ".krp-installed" ]; then
+    echo "Installing KRP Server..."
+    wine krp-installer.exe -extract
+    unzip krp.zip
+    rm krp-installer.exe
+    rm krp.zip
+    cat > .krp-installed
+    echo "Done..."
+else
+    echo "KRP Server already installed..."
+fi
+
 
 echo "Server started..."
 wine kart.exe -dedicated 54411
