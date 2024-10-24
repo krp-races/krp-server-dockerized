74ad04c060054f229eadc22a6cc616bba48d8283
Fynn Haupt <info@fynnhaupt.de>
2024-10-24 18:29:00 +0200

    removed windows container

--- scripts/start.ps1
+++
@@ -1,4 +0,0 @@
-echo "Server started..."
-echo "Config: $env:SERVER_CONFIG"
-Start-Process -FilePath "kart.exe" -ArgumentList "-dedicated","54411","-set params",$env:SERVER_CONFIG -Wait
-echo "Server stopped..."
